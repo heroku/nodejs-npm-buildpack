cfe0658e5e8193908e8ead81856609bc92f9ccb2
Jesse Brown <jabrown85@gmail.com>
2020-05-19 10:01:32 -0500

    feat: provide node_modules and require node in buildplan
    Provide `node_modules` so that buildpacks that require `node_modules` will be satisfied. Require `node` so that the detection phase will fail if no buildpack providing `node` was provided.
    
    https://github.com/projectriff/node-function-buildpack/blob/07eacdbe9fe49b8ce8f726fc617a5bc895b9a7b4/node/buildpack.go#L78
    
    Co-authored-by: Travis <longoria.public@gmail.com>

--- shpec/detect_shpec.sh
+++ shpec/detect_shpec.sh
@@ -32,4 +32,26 @@ describe "lib/detect.sh"
 
     rm -rf "$project_dir"
   end
+
+  describe "write_to_build_plan"
+    it "writes node and node_modules as expected in build plan"
+      project_dir=$(create_temp_project_dir)
+      touch "$project_dir/buildplan.toml"
+      write_to_build_plan "$project_dir/buildplan.toml"
+      actual=$(cat "$project_dir/buildplan.toml")
+      echo "$actual"
+      expected=$(cat <<EOF
+  [[provides]]
+  name = "node_modules"
+
+  [[requires]]
+  name = "node_modules"
+
+  [[requires]]
+  name = "node"
+EOF
+)
+      assert equal "$actual" "$expected"
+    end
+  end
 end
